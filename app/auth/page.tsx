ec98f693043744b2d6e13f88b571f5a61be6c27e
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2024-11-17 01:29:19 +0700

    add auth

---
+++ app/auth/page.tsx
@@ -0,0 +1,216 @@
+"use client";
+
+import toast from "react-hot-toast";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createClient } from "@/lib/supabase/client";
+
+const formLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+});
+
+const formRegisterSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters long",
+  }),
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+});
+
+export default function Page() {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const formLogin = useForm<z.infer<typeof formLoginSchema>>({
+    resolver: zodResolver(formLoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const formRegister = useForm<z.infer<typeof formRegisterSchema>>({
+    resolver: zodResolver(formRegisterSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmitLogin = async (values: z.infer<typeof formLoginSchema>) => {
+    setIsSubmitting(true);
+    toast.loading("Logging in...");
+
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: values.email,
+      password: values.password,
+    });
+
+    if (error) {
+      toast.dismiss();
+      toast.error(error.message);
+      setIsSubmitting(false);
+    } else {
+      return router.push("/dashboard");
+    }
+  };
+
+  const onSubmitRegister = async (values: z.infer<typeof formRegisterSchema>) => {
+    setIsSubmitting(true);
+    toast.loading("Signing up...");
+
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signUp({
+      email: values.email,
+      password: values.password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+        data: {
+          name: values.name,
+          email: values.email,
+          username: values.email.split('@')[0],
+        },
+      },
+    });
+
+    toast.dismiss();
+    if (error) {
+      console.error(error.code + " " + error.message);
+      toast.error(error.message);
+    } else {
+      toast.success("Thanks for signing up! Please check your email for a verification link.");
+    }
+    setIsSubmitting(false);
+  };
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Register</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Form {...formLogin}>
+            <form onSubmit={formLogin.handleSubmit(onSubmitLogin)}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Login</CardTitle>
+                  <CardDescription>Welcome back! Please log in to access your account.</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <FormField
+                    control={formLogin.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder="Your email address" disabled={isSubmitting} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={formLogin.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" placeholder="Your password" disabled={isSubmitting} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </CardContent>
+                <CardFooter>
+                  <Button type="submit" disabled={isSubmitting}>
+                    Submit
+                  </Button>
+                </CardFooter>
+              </Card>
+            </form>
+          </Form>
+        </TabsContent>
+        <TabsContent value="register">
+          <Form {...formRegister}>
+            <form onSubmit={formRegister.handleSubmit(onSubmitRegister)}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Register</CardTitle>
+                  <CardDescription>Sign up to take control of your financial future!</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <FormField
+                    control={formRegister.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder="Your name" disabled={isSubmitting} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={formRegister.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder="Your email address" disabled={isSubmitting} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={formRegister.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" placeholder="Your password" disabled={isSubmitting} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </CardContent>
+                <CardFooter>
+                  <Button type="submit" disabled={isSubmitting}>
+                    Submit
+                  </Button>
+                </CardFooter>
+              </Card>
+            </form>
+          </Form>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
